5f040fec9e64c96cc924842f57397000ca8fe999
Olya <olya.mashkov@yandex.ru>
2019-06-02 17:11:00 +0300

    hw5 - recursion

---
+++ src/main/java/recursion/Main.java
@@ -0,0 +1,40 @@
+package recursion;
+
+import stackpack.MyStack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(gradeFunc(3, 2));
+
+
+        MyStack<Integer> havoyStackA = new MyStack<>();
+        MyStack<Integer> havoyStackB = new MyStack<>();
+        MyStack<Integer> havoyStackC = new MyStack<>();
+        for (int i = 0; i < 3; i++) {
+            havoyStackA.push(i);
+        }
+        System.out.println(havoyStackA);
+        hanoy(havoyStackA, havoyStackB, havoyStackC, 3);
+        System.out.println(havoyStackC);
+    }
+
+    public static int gradeFunc(int a, int b) {
+        if (b == 1 || a == 1) {
+            return a;
+        }
+        return gradeFunc(a, b - 1) * a;
+    }
+
+
+    public static void hanoy(MyStack<Integer> a,
+                             MyStack<Integer> b, MyStack<Integer> c, int disks) {
+        if (disks == 1) {
+            c.push(a.pop());
+        } else {
+            hanoy(a, c, b, disks - 1);
+            c.push(a.pop());
+            hanoy(b, a, c, disks - 1);
+        }
+    }
+}
+
