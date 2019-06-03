374394aa04ba37ab41bd0e1f8ade5b7eeee3d2e2
Olya <olya.mashkov@yandex.ru>
2019-06-03 19:59:01 +0300

    hw5.2

--- src/main/java/recursion/Main.java
+++ src/main/java/recursion/Main.java
@@ -4,18 +4,18 @@ import stackpack.MyStack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(gradeFunc(3, 2));
+        System.out.println(gradeFunc(7, 6));
+        System.out.println(cycleFunc(7, 6));
 
-
-        MyStack<Integer> havoyStackA = new MyStack<>();
-        MyStack<Integer> havoyStackB = new MyStack<>();
-        MyStack<Integer> havoyStackC = new MyStack<>();
-        for (int i = 0; i < 3; i++) {
-            havoyStackA.push(i);
-        }
-        System.out.println(havoyStackA);
-        hanoy(havoyStackA, havoyStackB, havoyStackC, 3);
-        System.out.println(havoyStackC);
+//        MyStack<Integer> havoyStackA = new MyStack<>();
+//        MyStack<Integer> havoyStackB = new MyStack<>();
+//        MyStack<Integer> havoyStackC = new MyStack<>();
+//        for (int i = 0; i < 3; i++) {
+//            havoyStackA.push(i);
+//        }
+//        System.out.println(havoyStackA);
+//        hanoy(havoyStackA, havoyStackB, havoyStackC, 3);
+//        System.out.println(havoyStackC);
     }
 
     public static int gradeFunc(int a, int b) {
@@ -23,6 +23,14 @@ public class Main {
             return a;
         }
         return gradeFunc(a, b - 1) * a;
+    }
+
+    public static int cycleFunc(int a, int b) {
+        int temp = 1;
+        for (int i = 0; i < b; i++) {
+            temp *= a;
+        }
+        return temp;
     }
 
 
